18a7ada66042387b25406d95da98d8101cc7e56a
kawsar007 <imkawsar007@gmail.com>
2026-07-13 13:28:32 +0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h2 className="text-2xl font-bold">This is clean main page</h2>
+    </div>
+  );
+}
